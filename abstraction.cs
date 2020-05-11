c8173490aa29c9f1cbad73191ca72162fcbd92d8
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-05-11 16:12:09 -0600

    working on abstraction

--- abstraction.cs
+++ abstraction.cs
@@ -1,30 +1,46 @@
-// using System;
+using System;
 
-// namespace abstraction
-// {
-//     class Program
-//     {
-//         abstract class SecretMessage
-//         {
-//             public void mySecretMessage()
-//             {
-//                 Console.WriteLine("This is my secret message!");
-//             }
-//         }
+namespace abstraction
+{
+    class Program
+    {
+        abstract class Name
+        {
+            public void myName()
+            {
+                Console.WriteLine("Name: - He's on FIRE! - ");
+            }
+        }
 
-//         class PublicMessage : SecretMessage
-//         {
-//             public void myPublicMessage()
-//             {
-//                 Console.WriteLine("Everyone can see this message!");
-//             }
-//         }
-//         static void Main(string[] args)
-//         {
-//             PublicMessage myPubMsg = new PublicMessage();
-//             myPubMsg.myPublicMessage();
-//             myPubMsg.mySecretMessage();
+        abstract class Artist
+        {
+            public void myArtist()
+            {
+                Console.WriteLine("Artist: - He's on FIRE! - ");
+            }
+        }
+        abstract class Medium
+        {
+            public void myMedium()
+            {
+                Console.WriteLine("Floppy Disk!");
+            }
+        }
 
-//         }
-//     }
-// }
+        class PublicMessage : Name
+        {
+            public void myPublicMessage()
+            {
+                Console.WriteLine("Welcome to the Art Gallery!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            PublicMessage myPubMsg = new PublicMessage();
+            myPubMsg.myPublicMessage();
+            myPubMsg.myName();
+            myPubMsg.myMedium();
+
+        }
+    }
+}
